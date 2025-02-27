8edb874d6aca8d8bc2c48dfca94365a650c78854
Dain Park <zndgn555@gmail.com>
2025-02-27 22:53:17 +0100

    update

--- code/server/main.go
+++ code/server/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+func TrackIp(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{
+		"ip": r.RemoteAddr,
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
-	log.Println("Starting server on port 8080")
-	port := 8000
+	port := 8080
+	http.HandleFunc("/ping", TrackIp)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
